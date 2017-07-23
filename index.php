a9f314300098d17227d8a5ac94d9a1be797a56d5
sa947 <sa947@njit.edu>
2017-07-23 16:56:07 -0400

    edited index

--- index.php
+++ index.php
@@ -6,65 +6,186 @@ $message = '';
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action =  'start_app';
-}
+    }
 
 
-switch ($action) {
-    case 'start_app':
+    switch ($action) {
+        case 'start_app':
+	        
 
-       
-        $interval = new DateInterval('P1M');
-        $default_date = new DateTime();
-        $default_date->sub($interval);
-        $invoice_date_s = $default_date->format('n/j/Y');
+		        $message = 'Enter both dates in m-d-Y format and click
+			on the Submit button.';
+			        break;
+				    case 'process_data':
+				            $invoice_date_s =
+					    filter_input(INPUT_POST,
+					    'invoice_date');
+					            $due_date_s =
+						    filter_input(INPUT_POST,
+						    'due_date');
 
-       
-        $interval = new DateInterval('P2M');
-        $default_date = new DateTime();
-        $default_date->add($interval);
-        $due_date_s = $default_date->format('n/j/Y');
+						           
+							           if
+								   (empty($invoice_date_s)
+								   ||
+								   empty($due_date_s))
+								   {
+								               $message
+									       =
+									       'You
+									       must
+									       enter
+									       both
+									       dates.
+									       Please
+									       try
+									       again.';
+									                   break;
+											           }
 
-        $message = 'Enter two dates and click on the Submit button.';
-        break;
-    case 'process_data':
-        $invoice_date_s = filter_input(INPUT_POST, 'invoice_date');
-        $due_date_s = filter_input(INPUT_POST, 'due_date');
+												           
+													           try
+														   {
+														               
+															                   $invoice_date_o
+																	   =
+																	   DateTime::createFromFormat('m-d-Y',
+																	   $invoice_date_s);
+																	               $due_date_o
+																		       =
+																		       DateTime::createFromFormat('m-d-Y',
+																		       $due_date_s);
+																		                   
+																				               if
+																					       (!is_object($invoice_date_o)
+																					       ||
+																					       !is_object($due_date_o))
+																					       {
+																					                   throw
+																							   new
+																							   Exception('DateTime::createFromFormat
+																							   error');
+																							                   }
+																									           
+																										               }
+																											       catch
+																											       (Exception
+																											       $e)
+																											       {
+																											                   
+																													               $message
+																														       =
+																														       'Both
+																														       dates
+																														       must
+																														       be
+																														       in
+																														       a
+																														       valid
+																														       format
+																														       (m-d-Y).
+																														       Please
+																														       check
+																														       both
+																														       dates
+																														       and
+																														       try
+																														       again.';
+																														                   break;
+																																              }
 
-        
-        
-        if (empty($invoice_date_s) || empty($due_date_s)) {
-            $message = 'Dates not entered. Please enter the dates and try again.';
-            break;
-        }
+																																	              if
+																																		      ($due_date_o
+																																		      <
+																																		      $invoice_date_o)
+																																		      {
+																																		                  $message
+																																				  =
+																																				  'The
+																																				  due
+																																				  date
+																																				  must
+																																				  come
+																																				  after
+																																				  the
+																																				  invoice
+																																				  date.
+																																				  Please
+																																				  try
+																																				  again.';
+																																				              break;
+																																					              }
 
-        try {
-            $invoice_date_o = new DateTime($invoice_date_s);
-            $due_date_o = new DateTime($due_date_s);
-            } catch (Exception $e) {
-            $message = 'Dates must be in a valid format. Please check the dates and try again.';
-            break;
-        }
-     
-        
-        if ($due_date_o < $invoice_date_o) {
-            $message = 'The due date must come after the invoice date. Please try again.';
-            break;
-        }
-        
-         $format_string = 'F j, Y';
+																																						              date_default_timezone_set('America/New_York');
 
-     
-        $invoice_date_f = 'not implemented yet';
-        $due_date_f = 'not implemented yet'; 
-        
-      
-        $current_date_f = 'not implemented yet';
-        $current_time_f = 'not implemented yet';
-        
-       
-        $due_date_message = 'not implemented yet';
-
-        break;
-}
-include 'date_tester.php';
-?>
+																																							              $current_date_o
+																																								      =
+																																								      new
+																																								      DateTime();
+																																								              
+																																									              $time_span
+																																										      =
+																																										      $current_date_o->diff($due_date_o);
+																																										              if
+																																											      ($due_date_o
+																																											      <
+																																											      $current_date_o)
+																																											      {
+																																											                  $due_date_message
+																																													  =
+																																													  $time_span->format(
+																																													                  'This
+																																															  invoice
+																																															  is
+																																															  %y
+																																															  years,
+																																															  %m
+																																															  months,
+																																															  and
+																																															  %d
+																																															  days
+																																															  overdue.');
+																																															          }
+																																																  else
+																																																  {
+																																																              $due_date_message
+																																																	      =
+																																																	      $time_span->format(
+																																																	                      'This
+																																																			      invoice
+																																																			      is
+																																																			      due
+																																																			      in
+																																																			      %y
+																																																			      years,
+																																																			      %m
+																																																			      months,
+																																																			      and
+																																																			      %d
+																																																			      days.');
+																																																			              }
+																																																				              
+																																																					              $format_string
+																																																						      =
+																																																						      'F
+																																																						      j,
+																																																						      Y';
+																																																						              $invoice_date_f
+																																																							      =
+																																																							      $invoice_date_o->format($format_string);
+																																																							              $due_date_f
+																																																								      =
+																																																								      $due_date_o->format($format_string);
+																																																								              $current_date_f
+																																																									      =
+																																																									      $current_date_o->format($format_string);
+																																																									              $current_time_f
+																																																										      =
+																																																										      $current_date_o->format('g:i:s
+																																																										      a');
+																																																										              
+																																																											              break;
+																																																												      }
+																																																												      include
+																																																												      'date_tester.php';
+																																																												      ?>
